ebcafce8184cfc428cae4aa5467749e28e6eb189
Michael Oberrauch <bloor.mo@protonmail.com>
2020-02-22 15:37:32 +0100

    added rudementary gitignore and requirements.txt
    also exported token to ignored 'token' file

--- echo_bot.py
+++ echo_bot.py
@@ -1,16 +1,22 @@
 import telebot
 import time
 
-bot = telebot.TeleBot("939974039:AAExAMFIvn5zkSEx60I_bx4iXyYGbeLZn7g")
+def main():
+    with open("token") as f:
+        token = f.readline().strip()
+    bot = telebot.TeleBot(token)
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-	bot.reply_to(message, "hihi, bl, lol")
+    @bot.message_handler(commands=['start', 'help'])
+    def send_welcome(message):
+        bot.reply_to(message, "hihi, bl, lol")
 
-@bot.message_handler(commands=['hh'])
-def send_hh(message):
-    bot.reply_to(message, "Starting to hh. All members have to hh within 60 seconds. Else kick.")
-    time.sleep(60.0)
-    bot.reply_to(message, ":(")
+    @bot.message_handler(commands=['hh'])
+    def send_hh(message):
+        bot.reply_to(message, "Starting to hh. All members have to hh within 60 seconds. Else kick.")
+        time.sleep(60.0)
+        bot.reply_to(message, ":(")
 
-bot.polling()
+    bot.polling()
+
+if __name__ == "__main__":
+    main()
