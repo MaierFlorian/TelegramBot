d2ea1b35a237368e0c3deaffa46b3649b3a80692
MaierFlorian <51724993+MaierFlorian@users.noreply.github.com>
2020-02-22 16:10:56 +0100

    Update echo_bot.py

--- echo_bot.py
+++ echo_bot.py
@@ -1,20 +1,36 @@
 import telebot
 import time
 
+knownUsers = []  # todo: save these in a file,
+userStep = {}  # so they won't reset every time the bot restarts
+
 def main():
-    with open("token") as f:
+    with open("token.txt") as f:
         token = f.readline().strip()
     bot = telebot.TeleBot(token)
 
-    @bot.message_handler(commands=['start', 'help'])
-    def send_welcome(message):
-        bot.reply_to(message, "hihi, bl, lol")
+    @bot.message_handler(commands=['start'])
+    def command_start(m):
+        cid = m.chat.id
+        if cid not in knownUsers:  # if user hasn't used the "/start" command yet:
+            knownUsers.append(cid)  # save user id, so you could brodcast messages to all users of this bot later
+            userStep[cid] = 0  # save user id and his current "command level", so he can use the "/getImage" command
+            bot.send_message(cid, "Hello, stranger, let me scan you...")
+            bot.send_message(cid, "Scanning complete, I know you now")
+        else:
+            bot.send_message(cid, "I already know you, no need for me to scan you again!")
 
     @bot.message_handler(commands=['hh'])
     def send_hh(message):
         bot.reply_to(message, "Starting to hh. All members have to hh within 60 seconds. Else kick.")
         time.sleep(60.0)
         bot.reply_to(message, ":(")
+
+    @bot.message_handler(func=lambda m: True)
+    def echo_all(message):
+        if(message.text.lower() == "hh"):
+            bot.send_message(message.chat.id, "hh")
+            return message.chat.id
 
     bot.polling()
 
