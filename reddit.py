c47a936f715ef085d973ff7070d969938e33f41c
Florian Maier <1florian9@gmail.com>
2020-02-24 15:44:52 +0100

    cleaned up and changed hh function

--- reddit.py
+++ reddit.py
@@ -5,25 +5,7 @@ from prawcore import NotFound
 import re
 
 def init(bot):
-    #with open("token.txt") as f:
-    #    token = f.readline().strip()
-    #bot = telebot.TeleBot(token)
-
-    print("reading reddit")
-
-    @bot.message_handler(commands=['start'])
-    def send_welcome(message):
-	    bot.reply_to(message, "Moin")
-
-    @bot.message_handler(commands=['help'])
-    def send_help(message):
-	    bot.reply_to(message, "Available commands:\n\\r or \\reddit or \\meme")
-
-    @bot.message_handler(func=lambda message: message.text.lower() == "hh")
-    def echo_all(message):
-        bot.send_message(message.chat.id, "hh, " + str(message.chat.first_name))
-        return message.chat.id
-    
+  
     @bot.message_handler(commands=['r', 'reddit', 'meme'])
     def send_meme(m):
         args = m.text.split()
